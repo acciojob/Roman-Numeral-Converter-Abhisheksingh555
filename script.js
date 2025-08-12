afc1ea7acb7af9134b6ea2b3b1df4da7a0108af7
Abhishek Singh <136239184+Abhisheksingh555@users.noreply.github.com>
2025-08-12 20:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,22 @@
 function convertToRoman(num) {
-    const obj = {
-        0: ['M', 1000],
-        1: ['D', 500],
-        2: ['C', 100],
-        3: ['L', 50],
-        4: ['X', 10],
-        5: ['V', 5],
-        6: ['I', 1]
-    };
-
-    if (num === 0) return ""; // No roman numeral for 0
-
-    let result = "";
-
-    // Roman numeral special cases
-    const specials = {
-        900: 'CM', 400: 'CD', 90: 'XC', 40: 'XL', 9: 'IX', 4: 'IV'
-    };
-
-    // Merge specials into sequence for correct order
-    const values = [
-        [1000, 'M'],
-        [900, 'CM'],
-        [500, 'D'],
-        [400, 'CD'],
-        [100, 'C'],
-        [90, 'XC'],
-        [50, 'L'],
-        [40, 'XL'],
-        [10, 'X'],
-        [9, 'IX'],
-        [5, 'V'],
-        [4, 'IV'],
-        [1, 'I']
+    const romanNumerals = [
+        ['M', 1000],
+        ['D', 500],
+        ['C', 100],
+        ['L', 50],
+        ['X', 10],
+        ['V', 5],
+        ['I', 1]
     ];
-
-    for (let [value, symbol] of values) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
+    
+    let result = '';
+    
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (num >= romanNumerals[i][1]) {
+            result += romanNumerals[i][0];
+            num -= romanNumerals[i][1];
         }
     }
-
-    return result.toUpperCase();
+    
+    return result;
 }
-
-// Example test cases
-console.log(convertToRoman(14));   // XIV
-console.log(convertToRoman(798));  // DCCXCVIII
-console.log(convertToRoman(36));   // XXXVI
-
-module.exports = convertToRoman;
